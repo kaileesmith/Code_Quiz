37fe6d25c065d70c1aadd54ca02443394c154641
Kailee Smith <kaileesmith1501@gmail.com>
2020-10-19 22:41:09 -0700

    questions added to js with idea of functions

---
+++ app.js
@@ -0,0 +1,40 @@
+// var startBtn = document.querySelector("#startButton");
+var countDown = 75;
+var quizTime;
+
+// array for questions
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+];
+
+// function
+
+
+// Event Listener to Start Quiz
+
+// startBtn.addEventListener("click", beginQuiz);
+// console.log("hello")
